319d4d80308d89e777ca79b414ef5c288f162f64
Dicki Smits <dicki@absoluut.org>
2026-01-04 11:23:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -446,6 +446,7 @@ if (!is_dir('images')) {
     <script src="assets/js/admin-extensions.js"></script>
     <script src="assets/js/admin-datatable-loaders.js"></script>
     <script src="assets/js/admin-timeline-groups.js"></script>
+    <script src="assets/js/timeline-admin.js"></script>
    
 <?php endif; ?>
 </body>
